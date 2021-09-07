7a5fd4d5cc43a134ac5ef644407faec2152aec30
JONATHAN <hello@jonali.me>
2021-09-07 11:35:36 -0400

    feature/bootstrap-font add react-toastify for notifications

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import React from 'react';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-    </Switch>
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    <ToastContainer />
+  </>
 );
 
 export default App;
