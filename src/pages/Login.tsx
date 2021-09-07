fc77372f4f2862382f0aeeec79ef5e24461faf31
JONATHAN <hello@jonali.me>
2021-09-07 15:13:01 -0400

    feature/login add Login component design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button, Card, Col, Form, Row,
+} from 'react-bootstrap';
 import MainLayout from '../layouts/MainLayout';
 
-const Login: React.FC = () => (
-  <MainLayout>Login</MainLayout>
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-);
+  const onClick = () => {
+    console.log('TEST CLICK');
+  };
+
+  return (
+    <MainLayout>
+      <Row className="justify-content-center">
+        <Col lg={6} md={8}>
+          <Card>
+            <Card.Body>
+              <h3 className="text-center">
+                <b>LOGIN</b>
+              </h3>
+
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button variant="standard" onClick={onClick}>Sign In</Button>
+
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </MainLayout>
+  );
+};
 
 export default Login;
