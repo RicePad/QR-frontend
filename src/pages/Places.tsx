4d2b046983645af1980d254c8513fe65d3366ee2
JONATHAN <hello@jonali.me>
2021-09-09 11:11:04 -0400

    feature/context Set up context, login, sign-in, register and private route functionalities

---
+++ src/pages/Places.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+const Places: React.FC = () => (
+  <MainLayout>
+    <h1>Places</h1>
+  </MainLayout>
+);
+
+export default Places;
